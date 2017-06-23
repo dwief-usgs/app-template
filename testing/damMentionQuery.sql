aac05a1b44d8444dd096dba823d3c36662140e5d
Daniel Wieferich <dwieferich@usgs.gov>
2017-06-23 10:03:04 -0600

    Added removal terms to snorkel app, updated testing items link citation notebook

--- testing/damMentionQuery.sql
+++ testing/damMentionQuery.sql
@@ -1,1 +1,1 @@
-﻿select b.* from (select docid, sentid, unnest(words) as word from sentences) a left join sentences b on (a.docid=b.docid AND a.sentid=b.sentid) where (lower(a.word) = 'dam' OR lower(a.word) LIKE '%-dam' OR lower(a.word) LIKE 'dam-%') ;
+﻿select distinct(b.*) from (select docid, sentid, unnest(words) as word from sentences) a left join sentences b on (a.docid=b.docid AND a.sentid=b.sentid) where (lower(a.word) = 'dam' OR lower(a.word) LIKE '%-dam' OR lower(a.word) LIKE 'dam-%') ;
