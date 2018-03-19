da4907914ae1ed66bfdfea81d842a719f481ce4d
Brandon Serna <bserna@usgs.gov>
2018-03-19 14:23:54 -0600

    included dam list resource

--- app/utils.py
+++ app/utils.py
@@ -48,3 +48,32 @@ def connect_db():
     df = pd.read_sql_query('select docid, sentid, wordidx, words, poses, ners from sentences_nlp352;', con=conn)
     print('Sentences: %s' %len(df))
     return df
+
+
+# Loading related data
+def get_drip_resources(URL='https://beta-gc2.datadistillery.org/api/v1/sql/bcb?q=select * from drip.dripdams'):
+    """Export of known dam removals from data distillery
+    Parameters
+    ----------
+    URL : str
+        Built query for GC2.
+    """
+    try:
+        r = requests.get(URL)
+        if r.status_code == 200:
+            return r.json()
+        else:
+            raise Exception('Data Distillery URL returning: %s', r.status_code)
+    except Exception as e:
+        raise Exception(e)
+
+
+def get_dams():
+    # Load Data Distillery dam data
+    dams = pd.DataFrame([i['properties'] for i in get_drip_resources()['features']])
+    # Cleaned name list
+    # Rm river/dam and related words
+    dams['name'] = dams['dam_name'].str.replace('Unknown', '')
+    dams['name'] = dams['name'].dropna()
+    dams['name'] = dams['dam_name'].str.replace('Dam', '').replace('River', '').replace('River)', '').replace('dam', '')
+    return dams
